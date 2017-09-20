782f182ce8bb9666857b7cd1517366d5ef15d4b7
Yuya.Nishida <yuya@j96.org>
2017-09-20 20:45:40 +0900

    add asserts.

--- test/git_cococo_test.rb
+++ test/git_cococo_test.rb
@@ -48,6 +48,7 @@ class GitCococoTest < Test::Unit::TestCase
     exist_file_path.write("wrote.\n")
     @repository.git_commit(@repository.git_add(exist_file_path.basename))
     assert_equal(1, @repository.head.log.length)
+    assert_git_status([])
 
     new_file_path = @repository_path / "new_file.txt"
     command = "git cococo write_file #{new_file_path.basename} wrote."
@@ -55,6 +56,7 @@ class GitCococoTest < Test::Unit::TestCase
       run_command(command)
     end
 
+    assert_git_status([])
     assert_equal("run: #{command}\n", @repository.head.target.message)
     assert_equal(2, @repository.head.log.length)
     assert_equal("wrote.\n", new_file_path.read)
@@ -65,12 +67,14 @@ class GitCococoTest < Test::Unit::TestCase
     exist_file_path.write("wrote.\n")
     @repository.git_commit(@repository.git_add(exist_file_path.basename))
     assert_equal(1, @repository.head.log.length)
+    assert_git_status([])
 
     command = "git cococo write_file #{exist_file_path.basename} wrote."
     Dir.chdir(@repository_path) do
       run_command(command)
     end
 
+    assert_git_status([])
     assert_equal("run: #{command}\n", @repository.head.target.message)
     assert_equal(2, @repository.head.log.length)
     assert_equal("wrote.\nwrote.\n", exist_file_path.read)
