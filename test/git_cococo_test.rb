1793e578630f65532b646ce505fec80bbb7426d9
Yuya.Nishida <yuya@j96.org>
2017-10-02 23:32:49 +0900

    fix escaping command line arguments on run.

--- test/git_cococo_test.rb
+++ test/git_cococo_test.rb
@@ -81,6 +81,31 @@ class GitCococoTest < Test::Unit::TestCase
     assert_equal("wrote.\nwrote.\n", exist_file_path.read)
   end
 
+  test("commit with escaped command commit message") do
+    exist_file_path = @repository_path / "exist_file.txt"
+    exist_file_path.write("wrote.\n")
+    @repository.git_commit(@repository.git_add(exist_file_path.basename))
+    assert_equal(1, @repository.head.log.length)
+    assert_git_status([])
+
+    content = " \"'$PATH  # \\"
+    command = [
+      *%w"git cococo write_file",
+      exist_file_path.basename.to_s,
+      content,
+    ]
+    Dir.chdir(@repository_path) do
+      run_command(*command)
+    end
+
+    assert_git_status([])
+    expected_content = content.gsub("\'", "'\\\\''") # ' => '\''
+    assert_equal("run: #{command[0 .. -2].join(" ")} '#{expected_content}'\n",
+                 @repository.head.target.message)
+    assert_equal(2, @repository.head.log.length)
+    assert_equal("wrote.\n#{content}\n", exist_file_path.read)
+  end
+
   test("do nothing and exit 1 if uncommitted changes are exists") do
     exist_file_path = @repository_path / "exist_file.txt"
     exist_file_path.write("wrote.\n")
