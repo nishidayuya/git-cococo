053e82accf2e9cc0c25a4043162cc1c80e7b4e6a
Yuya.Nishida <yuya@j96.org>
2017-11-22 01:00:57 +0900

    add unit test.

---
+++ test/unit/escaping_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class EscapingTest < Test::Unit::TestCase
+  sub_test_case("escape_quote_argument") do
+    test("foo") do
+      command_path = (Pathname(__dir__) / "../../exe/git-cococo").expand_path
+      stdout, stderr, status = *Open3.capture3("sh", stdin_data: <<STDIN)
+. #{command_path}
+escape_quote_argument foo
+STDIN
+      assert_equal("foo", stdout)
+      assert_equal("", stderr)
+      assert_equal(0, status.exitstatus)
+    end
+
+    test("abc\'def") do
+      command_path = (Pathname(__dir__) / "../../exe/git-cococo").expand_path
+      stdout, stderr, status = *Open3.capture3("sh", stdin_data: <<STDIN)
+. #{command_path}
+escape_quote_argument "abc'def"
+STDIN
+      assert_equal("abc\'\\\'\'def", stdout)
+      assert_equal("", stderr)
+      assert_equal(0, status.exitstatus)
+    end
+  end
+end
