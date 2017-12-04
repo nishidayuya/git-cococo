eb65851b6baac13b46ae1d7149bb31bceac36023
Yuya.Nishida <yuya@j96.org>
2017-12-05 01:32:50 +0900

    add "EscapingTest.test_escaping".

--- test/unit/escaping_test.rb
+++ test/unit/escaping_test.rb
@@ -1,13 +1,17 @@
 require "test_helper"
 
 class EscapingTest < UnitTestCase
-  sub_test_case("escape_quote_argument") do
-    data(without_quote_charactor: %w[foo foo],
-         with_quote_charactor: %w[abc'def abc'\''def])
+  def self.test_escaping
     test("") do |data|
       arguments, expected = *data
       assert_equal(expected, capture2_command(*arguments))
     end
+  end
+
+  sub_test_case("escape_quote_argument") do
+    data(without_quote_charactor: %w[foo foo],
+         with_quote_charactor: %w[abc'def abc'\''def])
+    test_escaping
   end
 
   sub_test_case("escape_one_argument") do
@@ -18,10 +22,7 @@ class EscapingTest < UnitTestCase
          with_quote_charactor: %w[abc'def 'abc'\''def'],
          with_quote_charactor_in_begin_of_argument: %w['foo ''\''foo'],
          with_quote_charactor_in_end_of_argument: %w[foo' 'foo'\'''])
-    test("") do |data|
-      arguments, expected = *data
-      assert_equal(expected, capture2_command(*arguments))
-    end
+    test_escaping
   end
 
   sub_test_case("escape_command_line") do
@@ -29,10 +30,7 @@ class EscapingTest < UnitTestCase
          two_arguments: [%w[foo bar], "foo bar"],
          two_arguments_with_space_charactor: [["foo", "ba r"], "foo 'ba r'"],
          two_arguments_with_quote_charactor: [["foo", "ba'r"], "foo 'ba'\\''r'"])
-    test("") do |data|
-      arguments, expected = *data
-      assert_equal(expected, capture2_command(*arguments))
-    end
+    test_escaping
   end
 
   private
